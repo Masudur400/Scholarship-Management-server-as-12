d02b4796f01b32b2289d959ac59b7a9635a196ad
Masudur400 <md8004210@gmail.com>
2024-06-02 14:30:04 +0600

    last update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -52,6 +52,19 @@ async function run() {
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
+    })
+
+    // delete user 
+    app.delete('/users/:id',async(req, res) =>{
+      const id = req.params.id 
+      const query= {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // update user role 
+    app.patch('/users/admin/:id',async(req,res) =>{
+      
     })
 
 
